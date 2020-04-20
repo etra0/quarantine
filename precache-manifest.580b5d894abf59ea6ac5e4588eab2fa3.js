32716c18de23efaa204a934d64c5908d169a86e1
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-04-20 18:43:51 -0400

    Updates

---
+++ precache-manifest.580b5d894abf59ea6ac5e4588eab2fa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8a4ffaddc38aed2d41435d10e7ae78",
+    "url": "/quarantine/index.html"
+  },
+  {
+    "revision": "323dc44a55acaacfd314",
+    "url": "/quarantine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "184da7b9ac8516e374a3",
+    "url": "/quarantine/static/js/2.f583a491.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quarantine/static/js/2.f583a491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323dc44a55acaacfd314",
+    "url": "/quarantine/static/js/main.8eaace72.chunk.js"
+  },
+  {
+    "revision": "6fff5394b71a32562c3b",
+    "url": "/quarantine/static/js/runtime-main.e90ebf03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quarantine/static/media/logo.5d5d9eef.svg"
+  }
+]);
