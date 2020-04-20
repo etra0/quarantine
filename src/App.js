fcef4c7269ba30620b7ccfc49947573dae8d8b1c
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-04-20 18:42:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getParams = (location) => {
+    const searchParams = new URLSearchParams(location.search);
+    return {
+      q: searchParams.get('q') || ''
+    };
+  }
+
+  const DAY = 1000 * 60 * 60 * 24;
+  const today = new Date();
+  const quarantine = new Date("03/16/2020");
+  const quarantine_a = new Date("03/22/2020")
+
+  const n_days_a = Math.ceil((today - quarantine_a)/DAY);
+  const n_days = Math.ceil((today - quarantine)/DAY);
+
+  const { q } = getParams(window.location);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Días desde la cuarentena: { n_days }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { q === 'andrea' &&
+        <p>
+          Días desde la cuarentena para la <b>Alondrea</b>: { n_days_a }
+        </p>
+        }
       </header>
     </div>
   );
